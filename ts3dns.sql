e785a5c4c5130a335a8538c967bb05c377302aba
Lucas <godkiller_nt@hammermaps.de>
2015-02-16 20:43:43 +0100

    Updae MySQL Table * machine-id *

--- ts3dns.sql
+++ ts3dns.sql
@@ -3,18 +3,12 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Feb 04, 2015 at 09:27 PM
+-- Generation Time: Feb 16, 2015 at 07:39 PM
 -- Server version: 10.0.12-MariaDB
 -- PHP Version: 5.6.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `ts3dns`
@@ -30,16 +24,17 @@ CREATE TABLE IF NOT EXISTS `dns` (
 `id` int(11) NOT NULL,
   `dns` varchar(250) NOT NULL DEFAULT '',
   `ip` varchar(50) NOT NULL DEFAULT '000.000.000.000:0000',
-  `default` int(1) NOT NULL DEFAULT '0'
+  `default` int(1) NOT NULL DEFAULT '0',
+  `machine-id` int(5) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `dns`
 --
 
-INSERT INTO `dns` (`id`, `dns`, `ip`, `default`) VALUES
-(1, 'reverse.privatedns.com', '123.123.123.123:2222', 0),
-(2, 'gggg.ggg1234.fr', '111.111.111.111:2346', 1);
+INSERT INTO `dns` (`id`, `dns`, `ip`, `default`, `machine-id`) VALUES
+(1, 'reverse.privatedns.com', '123.123.123.123:2222', 0, 0),
+(2, 'gggg.ggg1234.fr', '111.111.111.111:2346', 1, 0);
 
 --
 -- Indexes for dumped tables
@@ -60,6 +55,3 @@ ALTER TABLE `dns`
 --
 ALTER TABLE `dns`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
